66dd6659917223cd0809e02ec55c9a42b9fd5003
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-06-06 16:51:53 -0400

    color into component

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import RemoveColor from './RemoveColor';
 import './style/Color.css';
 
-function Color() {
-  return (
-    <div className="Color">
-      <p>Color</p>
-      <RemoveColor />
-    </div>
-  );
+class Color extends Component{
+  render(){
+  	return (
+      <div className="Color">
+        <p>Color</p>
+        <RemoveColor />
+      </div>
+    );
+  }
 }
 
 export default Color;
