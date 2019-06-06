2bce4e046f8f922835da47f458924d18a8eb6eb6
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-06-06 11:43:33 -0400

    initilalized quilt grid and cells as components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style/Cell.css';
 
-function Cell() {
-  return (
-    <div className="Cell">
-    </div>
-  );
+class Cell extends Component {
+	render(){
+		const colors = ['darkslategray', 'hotpink', 'limegreen', 'slateblue']
+	  return (
+	    <div 
+	    	className="Cell" 
+	    	style={{backgroundColor: colors[this.props.colorCount % colors.length]}}
+	    	onClick={() => this.props.changeColor()}
+	    >
+	    </div>
+	  )
+	}
 }
 
 export default Cell;
--- src/components/QuiltGrid.js
+++ src/components/QuiltGrid.js
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Cell from './Cell';
 import DimensionsSelect from './DimensionsSelect';
 import ColorList from './ColorList';
 import StyleOutput from './StyleOutput';
 import './style/QuiltGrid.css';
 
-function QuiltGrid() {
-  return (
-    <div className="QuiltGrid">
-    	<div className="gridEditor">
-	      <div className="grid">
-	      	<Cell />
-	      	<Cell />
-	      	<Cell />
-	      	<Cell />
+class QuiltGrid extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+    	colorCounters: [0, 1, 2, 3]
+    };
+  }
+
+	// click function for the color cell
+	changeColor(i){
+		let colorCounters = this.state.colorCounters.slice()
+		colorCounters[i] = colorCounters[i] + 1
+
+		this.setState({colorCounters : colorCounters})
+	}
+
+	render(){
+		return (
+	    <div className="QuiltGrid">
+	    	<div className="gridEditor">
+		      <div className="grid">
+		      	<Cell 
+		      		colorCount = {this.state.colorCounters[0]}
+		      		changeColor = {() => this.changeColor(0)}
+		      	/>
+		      	<Cell 
+		      		colorCount = {this.state.colorCounters[1]}
+		      		changeColor = {() => this.changeColor(1)}
+		      	/>
+		      	<Cell 
+		      		colorCount = {this.state.colorCounters[2]}
+		      		changeColor = {() => this.changeColor(2)}
+		      	/>
+		      	<Cell 
+		      		colorCount = {this.state.colorCounters[3]}
+		      		changeColor = {() => this.changeColor(3)}
+		      	/>
+		      </div>
 	      </div>
-      </div>
 
-      <div className="userInput">
-      	<DimensionsSelect />
-      	<DimensionsSelect />
+	      <div className="userInput">
+	      	<DimensionsSelect />
+	      	<DimensionsSelect />
 
-      	<ColorList />
+	      	<ColorList />
 
-      	<StyleOutput />
-      </div>
-    </div>
-  );
+	      	<StyleOutput />
+	      </div>
+	    </div>
+	  ); 
+	}
 }
 
 export default QuiltGrid;
