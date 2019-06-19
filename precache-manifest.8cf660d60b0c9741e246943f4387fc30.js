6ec966441b29a299453e518082dcd17ff43fe7b7
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-06-19 14:53:27 -0400

    Updates

---
+++ precache-manifest.8cf660d60b0c9741e246943f4387fc30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b9c6da2a4a94b6cdb559771a8082d9",
+    "url": "/QuiltGrid/index.html"
+  },
+  {
+    "revision": "7d35b7d3a0c5a6af64dc",
+    "url": "/QuiltGrid/static/css/main.000c8d7c.chunk.css"
+  },
+  {
+    "revision": "5136c6461daf9c524fe8",
+    "url": "/QuiltGrid/static/js/2.83199d70.chunk.js"
+  },
+  {
+    "revision": "7d35b7d3a0c5a6af64dc",
+    "url": "/QuiltGrid/static/js/main.c8317017.chunk.js"
+  },
+  {
+    "revision": "e8d988289aa462b39ab0",
+    "url": "/QuiltGrid/static/js/runtime~main.55b16fa6.js"
+  }
+]);
